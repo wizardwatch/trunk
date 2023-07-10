ddf0804cd00c75374a49cd4e08bd1448040b6847
Wyatt Osterling <wyatt.osterling@wizardwatch.net>
2023-07-10 04:07:58 -0400

    Added some code I forgor

--- common.nix
+++ common.nix
@@ -1,5 +1,8 @@
 { config, pkgs, lib, useOptionalModule, ...}:
-
+with lib;                      
+let
+  cfg = config.custom.isDev;
+in 
 {
     options.custom.isDev = {
         imports = ./arduino.nix;
